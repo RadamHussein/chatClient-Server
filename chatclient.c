e40276c058d55ed91358b993fd9ab53ba3b0444b
Adam Smith <stupidalphabet@gmail.com>
2017-02-08 18:13:12 -0600

    added input checking functions in chatclient

--- chatclient.c
+++ chatclient.c
@@ -7,16 +7,32 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+/*
+* This function prints an error message when called.
+* The error is to alert the user that their input
+* for the user handle was invalid"
+*/
+void displayUserHandleInputError(){
+	printf("Error: Your handle must be 10 characters or less\n");
+}
+
+/*
+* This function prints an error message when called.
+* The error is to alert the user that their input
+* for the message was invalid"
+*/
+void displayUserMessageInputError(){
+	printf("Error: Your message must be 500 characters or less\n");
+}
 
 /*
 * This function checks the lenght of user input
 * It returns 0 if the input is bad and 1 if it 
 * is good.
 */
-int checkHandleLength(char *input, int maxLength){
+int checkInputLength(char *input, int maxLength){
 	if (strlen(input) > maxLength){
-		printf("Error: Your handle must be 10 characters or less!\n");
-
+		//printf("Error: Your handle must be 10 characters or less!\n");
 		return 0;
 	}
 	else{
@@ -27,10 +43,10 @@ int checkHandleLength(char *input, int maxLength){
 /*
 * This function gets input from the user that will
 * server as their on screen handle.
-* It calls the checkHandleLength function to perform
+* It calls the checkInputLength function to perform
 * validation on the input from the user.
 */
-void getUserInput(char *input){
+void getUserHandle(char *input){
 	int validInput = 0;
 	int input_length_limit = 10;
 
@@ -39,8 +55,42 @@ void getUserInput(char *input){
 		printf("Enter your handle (up to 10 characters): ");
 		fgets(input, 21, stdin);
 
-		validInput = checkHandleLength(input, input_length_limit);
+		//check the length of the input
+		validInput = checkInputLength(input, input_length_limit);
+
+		//if input is invalid, print error
+		if (validInput == 0){
+			displayUserHandleInputError();
+		}
 	}
+}
+
+/*
+* This function gets input from the user that will
+* server as the message to be sent to the server.
+* It calls the checkInputLength function to perform
+* validation on the input from the user.
+*/
+void getUserMessage(char *prompt, char *message_with_handle){
+	int validInput = 0;
+	int input_length_limit = 501;
+	char message[1024];
+
+	while (validInput == 0){
+
+		printf("%s ", prompt);										//get message from user
+		fgets(message, 1024, stdin);
+
+		//check the length of the input
+		validInput = checkInputLength(message, input_length_limit);
+
+		//if input is invalid, print error
+		if (validInput == 0){
+			displayUserMessageInputError();
+		}
+	}
+		sprintf(message_with_handle, "%s %s", prompt, message);		//prepend handle to message
+	
 }
 
 int main(int argc, char *argv[])
@@ -56,7 +106,7 @@ int main(int argc, char *argv[])
 	char returnBuffer[514];					//array to hold string response received from server
 	char authenticator[2] = {'#', '\0'};	//a string that should match the authentication message sent from the server
 	char handle[20];						//a string to hold the user's handle
-	char message[501];						//a string containing the user's message
+	//char message[501];						//a string containing the user's message
 	char prompt[20];						//a string containing the prompt for user entry
 	char message_with_handle[514];			//a string containing the message with the handle prepended
 
@@ -82,7 +132,7 @@ int main(int argc, char *argv[])
 
 	//greet user and get a handle
 	printf("Welcome!\n");
-	getUserInput(handle);
+	getUserHandle(handle);
 
 	printf("Your hanlde will be %s \n", handle);
 	handle[strcspn(handle, "\n")] = '\0'; //Remove the trailing \n 
@@ -90,10 +140,11 @@ int main(int argc, char *argv[])
 
 	//loop accepting messages until user wishes to quit
 	while (quit == 0){
-		//printf("Enter a message: ");								//get message from user
-		printf("%s ", prompt);										//get message from user
-		fgets(message, 501, stdin);
-		sprintf(message_with_handle, "%s %s", prompt, message);		//prepend handle to message
+		//printf("%s ", prompt);										//get message from user
+		//fgets(message, 501, stdin);
+		//sprintf(message_with_handle, "%s %s", prompt, message);		//prepend handle to message
+
+		getUserMessage(prompt, message_with_handle);
 
 		// Send message to server
 		charsWritten = send(socketFD, message_with_handle, strlen(message_with_handle), 0); // Write to the server
@@ -116,33 +167,6 @@ int main(int argc, char *argv[])
 		memset(returnBuffer, '\0', sizeof(returnBuffer)); // Clear out the buffer again for reuse
 
 	}
-
-/*
-	//send authentication message to server
-	charsWritten = send(socketFD, authenticator, strlen(authenticator), 0); // Write to the server
-	if (charsWritten < 0) {
-		fprintf(stderr, "CLIENT ERROR writing to socket\n");
-	}
-
-	//check for completion
-	if (charsWritten < strlen(authenticator)) {
-		fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
-	}
-
-	// Get return message from server
-	memset(returnBuffer, '\0', sizeof(returnBuffer)); // Clear out the buffer again for reuse
-	charsRead = recv(socketFD, returnBuffer, sizeof(returnBuffer) - 1, 0); // Read data from the socket, leaving \0 at end
-	if (charsRead < 0) { fprintf(stderr, "CLIENT: ERROR reading from socket\n"); }
-	if (returnBuffer[0] != '#'){
-		//print rejection message from server
-		fprintf(stderr, "ERROR: Client is not authorized to connect.\n");
-		exit(1);
-	}
-
-	buffer[strcspn(buffer, "\n")] = '\0'; // Remove trailing \n from buffer
-*/
-
-	
 	
 	return 0;
 }
