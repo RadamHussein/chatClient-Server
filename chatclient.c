8218bbcb7beb264fdbf243b50d6b1f7800f7f356
Adam Smith <smithada@flip3.engr.oregonstate.edu>
2017-02-12 19:49:39 -0800

    final working version

--- chatclient.c
+++ chatclient.c
@@ -1,3 +1,18 @@
+/************************************************************
+* Project 1 - chatclient.c
+* Adam Smith
+* CS372 - Winter 2017
+*
+* Description: This is a chat client program. It can be used
+* to exchange messages with a server program running elsewhere.
+* This program requires 2 command line arguments. First the
+* <hostname> of the chat server, and second the <port number> of 
+* the chat server. The program allows more messages with lengths 
+* of up to 500 character. To exit the program and close the 
+* connection between the client and server, type "\quit" and 
+* send it as your last message. 
+************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -26,13 +41,12 @@ void displayUserMessageInputError(){
 }
 
 /*
-* This function checks the lenght of user input
+* This function checks the lenght of user input.
 * It returns 0 if the input is bad and 1 if it 
 * is good.
 */
 int checkInputLength(char *input, int maxLength){
 	if (strlen(input) > maxLength){
-		//printf("Error: Your handle must be 10 characters or less!\n");
 		return 0;
 	}
 	else{
@@ -42,7 +56,7 @@ int checkInputLength(char *input, int maxLength){
 
 /*
 * This function gets input from the user that will
-* server as their on screen handle.
+* serve as their on screen handle.
 * It calls the checkInputLength function to perform
 * validation on the input from the user.
 */
@@ -71,14 +85,18 @@ void getUserHandle(char *input){
 * It calls the checkInputLength function to perform
 * validation on the input from the user.
 */
-void getUserMessage(char *prompt, char *message_with_handle){
+void getUserMessage(char *prompt, char *validMessage){
 	int validInput = 0;
 	int input_length_limit = 501;
 	char message[1024];
 
+	//loop until input is valid
 	while (validInput == 0){
 
-		printf("%s ", prompt);										//get message from user
+		memset(message, '\0', sizeof(message)); // Clear out the buffer again for reuse
+		
+		//get message from user
+		printf("%s ", prompt);
 		fgets(message, 1024, stdin);
 
 		//check the length of the input
@@ -89,8 +107,114 @@ void getUserMessage(char *prompt, char *message_with_handle){
 			displayUserMessageInputError();
 		}
 	}
-		sprintf(message_with_handle, "%s %s", prompt, message);		//prepend handle to message
-	
+
+	//copy validated message into the validMessage string
+	sprintf(validMessage, "%s", message);	
+}
+
+/*
+* This function sets up and opens
+* the client socket.
+*/
+void setUpSocket(int *sockFD){
+	// Set up the socket
+	*sockFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (sockFD < 0) {
+		fprintf(stderr, "CLIENT: ERROR opening socket\n");
+	}
+}
+
+/*
+* This function connects the socket to the server
+* address struct.
+*/
+void connectToServer(int *sockFD, struct sockaddr_in serverAddress){
+	if (connect(*sockFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) { // Connect socket to address
+		fprintf(stderr, "CLIENT: ERROR connectiong\n");
+	}
+}
+
+/*
+* This function handles sending messages to the server
+* from the client. It returns the number of chars
+* written into the connection to check for completion.
+*/
+int sendMessage(int *sockFD, int charsWritten, char *message){
+	charsWritten = send(*sockFD, message, strlen(message), 0); // Write to the server
+	if (charsWritten < 0) {
+		fprintf(stderr, "CLIENT ERROR writing to socket\n");
+	}
+
+	return charsWritten;
+}
+
+/*
+* This function checks that the entire
+* message was sent by comparing the charsWritten
+* variable to the message length.
+*/
+void checkForCompletion(int charsWritten, char *message){
+	if (charsWritten < strlen(message)) {
+			fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+		}
+}
+
+/*
+* This function receives the message sent back from the 
+* server. 
+*/
+void receiveMessage(int *sockFD, int charsRead, char *message){
+	charsRead = recv(*sockFD, message, sizeof(message) - 1, 0); // Read data from the socket, leaving \0 at end
+	if (charsRead < 0) { fprintf(stderr, "CLIENT: ERROR reading from socket\n"); }
+}
+
+/*
+* This function takes the message the user entered,
+* compares it to the hard coded "\quit" string,
+* and returns a 1 if equal and a 0 if not equal.
+*/
+int checkForQuit(char *message){
+	char quit[] = "\\quit\n";					//string to compare
+	int equalsQuit = strcmp(quit, message);		//compare strings
+	if (equalsQuit == 0){
+		return 1;								
+	}
+	else{
+		return 0;
+	}
+}
+
+/*
+* This function checks the message sent back from the server to
+* see if it is a "\quit" message. If it is, this function
+* will close the connection.
+*/
+int checkForServerQuitMessage(char *message){
+	char quit[] = "\\quit";						//string to compare
+	int equalsQuit = strcmp(quit, message);		//compare strings
+	if (equalsQuit == 0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+//this function will close the socket connection 
+void handleQuit(int *sockFD, char *message){
+	int charsWritten;
+
+	printf("Goodbye!\n");
+
+	//remove the trailing \n before sending
+	message[strcspn(message, "\n")] = '\0';
+
+	//send a goodbye message to the server
+	charsWritten = sendMessage(sockFD, charsWritten, message);
+	checkForCompletion(charsWritten, message);
+
+	//close the connection
+	close(*sockFD);
 }
 
 int main(int argc, char *argv[])
@@ -104,31 +228,34 @@ int main(int argc, char *argv[])
 	struct hostent* serverHostInfo;
 
 	char returnBuffer[514];					//array to hold string response received from server
+	char message[501];
 	char authenticator[2] = {'#', '\0'};	//a string that should match the authentication message sent from the server
 	char handle[20];						//a string to hold the user's handle
-	//char message[501];						//a string containing the user's message
 	char prompt[20];						//a string containing the prompt for user entry
 	char message_with_handle[514];			//a string containing the message with the handle prepended
 
-	
+	//check for both required command line arguments
+	if (argv[1] == NULL || argv[2] == NULL){
+		perror("Invalid arguments!\n");
+		exit(0);
+	}
+
 	// Set up the server address struct
-	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[2]); // Get the port number, convert to an integer from a string
-	serverAddress.sin_family = AF_INET; // Create a network-capable socket
-	serverAddress.sin_port = htons(portNumber); // Store the port number
-	serverHostInfo = gethostbyname(argv[1]);	//ip is localhost
-	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); 	// Clear out the address struct
+	portNumber = atoi(argv[2]); 									// Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; 							// Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); 					// Store the port number
+	serverHostInfo = gethostbyname(argv[1]);						//ip is localhost
+	if (serverHostInfo == NULL) { 
+		fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); 
+	}
 	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
 
-	// Set up the socket
-	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	if (socketFD < 0) {
-		fprintf(stderr, "CLIENT: ERROR opening socket\n");
-	}
+	//set up the socket
+	setUpSocket(&socketFD);
+
 	// Connect to server
-	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) { // Connect socket to address
-		fprintf(stderr, "CLIENT: ERROR connectiong\n");
-	}	
+	connectToServer(&socketFD, serverAddress);
 
 	//greet user and get a handle
 	printf("Welcome!\n");
@@ -140,31 +267,57 @@ int main(int argc, char *argv[])
 
 	//loop accepting messages until user wishes to quit
 	while (quit == 0){
-		//printf("%s ", prompt);										//get message from user
-		//fgets(message, 501, stdin);
-		//sprintf(message_with_handle, "%s %s", prompt, message);		//prepend handle to message
+	
+		//have user enter a message
+		getUserMessage(prompt, message);
 
-		getUserMessage(prompt, message_with_handle);
+		//check if the user entered "\quit"
+		quit = checkForQuit(message);
 
-		// Send message to server
-		charsWritten = send(socketFD, message_with_handle, strlen(message_with_handle), 0); // Write to the server
-		if (charsWritten < 0) {
-			fprintf(stderr, "CLIENT ERROR writing to socket\n");
+		//if the user wants to quit
+		if (quit == 1){
+			//close the connection and quit
+			handleQuit(&socketFD, message);
+
+			//get out of the loop
+			break;
 		}
 
-		//check for completion
-		if (charsWritten < strlen(message_with_handle)) {
-			fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
-		}
+		//prepend handle to message for sending
+		sprintf(message_with_handle, "%s %s", prompt, message);
+
+		//Remove the trailing \n before sending
+		message_with_handle[strcspn(message_with_handle, "\n")] = '\0';  
+
+		//clear out message string for reuse
+		memset(message, '\0', sizeof(message));
+
+		//send message to server
+		charsWritten = sendMessage(&socketFD, charsWritten, message_with_handle);
+
+		//check that charsWritten == message length
+		checkForCompletion(charsWritten, message_with_handle);
 
 		// Get return message from server
 		memset(returnBuffer, '\0', sizeof(returnBuffer)); // Clear out the buffer again for reuse
 		charsRead = recv(socketFD, returnBuffer, sizeof(returnBuffer) - 1, 0); // Read data from the socket, leaving \0 at end
 		if (charsRead < 0) { fprintf(stderr, "CLIENT: ERROR reading from socket\n"); }
 
-		printf("Server > %s\n", returnBuffer);			  //display returned message
+		//see if the server sent a quit message
+		quit = checkForServerQuitMessage(returnBuffer);
 
-		memset(returnBuffer, '\0', sizeof(returnBuffer)); // Clear out the buffer again for reuse
+		//if server quit, close connection
+		if (quit == 1){
+			printf("Server closed the connection...\n");
+			close(socketFD);
+			exit(0);
+		}
+
+		//display returned message
+		printf("Server > %s\n", returnBuffer);
+
+		// Clear out the buffer again for reuse
+		memset(returnBuffer, '\0', sizeof(returnBuffer)); 
 
 	}
 	
--- chatserve.py
+++ chatserve.py
@@ -1,38 +1,155 @@
+'''
+Project 1 - chatserve.py
+Adam Smith
+CS372 - Winter 2017
+
+Description: This is a chat server program. When executed, 
+it will open a server socket, listening for a TCP connection request. 
+This program will allow the exchange of messages between a client
+application. It allows for messages of up to 500 characters in length.
+If the user of the server application wants to terminate a client connection,
+this can be done by sending "\quit" in a message to the client. 
+To quit the server all together, enter ctrl c on the keyboard.
+'''
 import socket
 import sys
+
+#this function is used for binding the socket to the port number.
+#It takes a socket object as an argument. 
+def BindSocket(socket):
+	try:
+		serverSocket.bind(('', serverPort))
+	except socket.error as msg:
+		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
+
+#this function starts the server listening on the specified port
+#It takes a socket object as an argument.
+def ListenSocket(socket):
+	serverSocket.listen(1)
+	print 'Server is listening on port #: %d' %serverPort
+
+#this function is used to establish a connection with a client
+#It takes a socket object as an argument
+def ConnectToClient(socket):
+	connectionSocket, addr = serverSocket.accept()
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	print '\n'
+	return connectionSocket
+
+#this function is used to receive messages from the client
+#It takes a connection object as an argument. 
+def ReceiveMessage(connectionSocket):
+	message = connectionSocket.recv(1024)
+	return message
+
+#this function is used to get input from the user. This will
+#be the response message sent back to the client.
+def GetResponseMessage():
+	messageTooLong = True
+
+	#loop until user enters a message that is less than 500 characters in length
+	while messageTooLong == True:
+
+		response = raw_input("Server > ")
+
+		#check the length of the message
+		messageTooLong = VerifyMessageLength(response)
+
+	return response
+
+'''
+this function is used to check that the user 
+entered a message of 500 characters or less.
+If the message is too long it returns true, 
+else it returns false. It takes a string as 
+as argument.
+'''
+def VerifyMessageLength(message):
+	messageLength = len(message)
+	if messageLength > 500:
+		print "Your message must be 500 character or less!"
+		return True
+	else:
+		return False
+
+#this function is used to send a message back to the client
+#It takes a connection object and a string as arguments.
+def SendMessage(connectionSocket, response):
+	connectionSocket.send(response)
+
+#this function compares the message entered to a string the user
+#would enter to quit the program. It returns true if they are
+#equal (the user wishes to quit) and false if not equal (the user
+#does not wish to quit)
+def CheckForQuit(message):
+	quit = "\quit"
+	if message == quit:
+		return True
+	else:
+		return False
 
 #parse command line argument to integer
 serverPort = int(sys.argv[1])
 
+#create a socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Bind socket
-try:
-	serverSocket.bind(('', serverPort))
-except socket.error as msg:
-	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	sys.exit()
+#bind the socket
+BindSocket(serverSocket)
 
 #Start listening on socket	
-serverSocket.listen(1)
-print 'Server started on HOSTNAME: %s' % socket.gethostbyname(socket.gethostname()) + ' Port: %d' % serverPort
-#print "HOSTNAME: %s" % socket.gethostbyname(socket.gethostname())
+ListenSocket(serverSocket)
 
-while True:
-	connectionSocket, addr = serverSocket.accept()
-	print 'Connected with ' + addr[0] + ':' + str(addr[1])
-	
-	try:
-		while True:
-			clientMessage = connectionSocket.recv(1024)
-			if clientMessage:
-				print '%s' % clientMessage
-				serverMessage = raw_input("Server > ")
-				print 'Server > %s' % serverMessage
-				connectionSocket.send(serverMessage)
-			else: 
-				print >>sys.stderr, 'no more data from client'
-				break
-	finally:
-		connectionSocket.close()
+try:
+	while True:
+		#connect to client 
+		connection = ConnectToClient(serverSocket)
+		
+		try:
+			while True:
+				#receive message from client
+				clientMessage = ReceiveMessage(connection)
+
+				if clientMessage:
+					#check if client's message is a quit message
+					quit = CheckForQuit(clientMessage)
+
+					#display received message
+					print '%s' % clientMessage
+
+					#if client sent "\quit" terminate connection
+					if quit == True:
+						print "closing connection..."
+						connection.close()
+						break
+
+					#get a response message from the user
+					serverMessage = GetResponseMessage()
+
+					#check if user wishes to quit
+					quit = CheckForQuit(serverMessage)
+
+					#if the user wants to quit, close the connection
+					if quit == True:
+						SendMessage(connection, serverMessage)
+						connection.close()
+						print "closing connection..."
+						break
+
+					#sent response message back to client
+					SendMessage(connection, serverMessage)
+				else: 
+					print >>sys.stderr, 'no more data from client'
+					connection.close()
+					break
+		except:
+			connection.close()
+			serverSocket.close()
+except:
+	connection.close()
+	serverSocket.close()
+	print '\n'
+	print 'Goodbye!\n'
+
 	
